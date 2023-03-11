76eb79ef8ae3e0111785095d91f47ce6d6596b4f
Brix <112671061+Azethyst@users.noreply.github.com>
2023-03-11 15:27:08 -0600

    Added the home Page
    Home page now contains a profile and statistics section. All data will be used later for search and messaging.

---
+++ frontend/src/Home.js
@@ -0,0 +1,27 @@
+import "./Home.css";
+import { FiUpload } from "react-icons/fi";
+
+const Home = () => {
+    return (
+        <div className="content">
+            <div className="section-profile">
+                <h1 className="section-profile-title">Profile</h1>
+                <div className="profile-icon"></div>
+                <div className="profile-name">Name: </div>
+                <div className="profile-id">Profile-ID: </div>
+                <textarea name="" id="" cols="30" rows="10" className="profile-description"placeholder="About Me..."></textarea>
+                <button className="update-description">
+                    <FiUpload/>
+                    </button>
+            </div>
+            <div className="section-stats">
+                <h1 className="section-stats-title">Stats</h1>
+                <div className="number-posts">Number of Posts: 35</div>
+                <div className="number-likes">Number of Likes: 20</div>
+                <div className="number-dislikes">NUmber of Dislikes: 1000</div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
