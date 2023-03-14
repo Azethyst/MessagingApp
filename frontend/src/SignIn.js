90b4eb1f63dffa22997087b7ef7ef6e5b8082c3d
Brix Ian Marcellana <marcellana_brix@yahoo.com>
2023-03-13 20:29:02 -0600

    Update
    Finished login Ui Screen

--- frontend/src/SignIn.js
+++ frontend/src/SignIn.js
@@ -1,14 +1,24 @@
 import "./SignIn.css";
 import { RiAliensFill } from "react-icons/ri";
+import { FiUser, FiLock } from "react-icons/fi";
 
 const SignIn = () => {
   return (
     <div>
-        <div className="content">
+      <div className="content">
         <h1 className="heading-title">Hello, Welcome To PlanetCord...</h1>
-        <h1 className="alien-icon"><RiAliensFill /> Hello from Sign In</h1>
-        
+        <div className="box">
+          <RiAliensFill className="alien-icon" />
+          <div className="form">
+            <FiUser className="form-icon" />
+            <input type="text" placeholder="Profile ID" className="input-box" />
+          </div>
+          <div className="form">
+            <FiLock className="form-icon" />
+            <input type="text" placeholder="Password" className="input-box" />
+          </div>
         </div>
+      </div>
     </div>
   );
 };
--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -1,14 +1,40 @@
 import "./SignUp.css";
 import { RiAliensFill } from "react-icons/ri";
+import { FiUser, FiLock } from "react-icons/fi";
 
 const SignUp = () => {
   return (
     <div>
-        <div className="content">
+      <div className="content">
         <h1 className="heading-title">Hello, Welcome To PlanetCord...</h1>
-        <h1 className="alien-icon"><RiAliensFill /> Hello from Sign up</h1>
-        
+        <div className="box">
+          <RiAliensFill className="alien-icon" />
+          <div className="sign-up-form">
+            <FiUser className="form-icon" />
+            <input
+              type="text"
+              placeholder="Name"
+              className="sign-up-input-box"
+            />
+          </div>
+          <div className="sign-up-form">
+            <FiUser className="form-icon" />
+            <input
+              type="text"
+              placeholder="Profile ID"
+              className="sign-up-input-box"
+            />
+          </div>
+          <div className="sign-up-form">
+            <FiLock className="form-icon" />
+            <input
+              type="text"
+              placeholder="Password"
+              className="sign-up-input-box"
+            />
+          </div>
         </div>
+      </div>
     </div>
   );
 };
